a3b0f6db943ce8c7b237ff29a3e73e941fcb9a85
Adithya-1489181 <adithya1489181@gmail.com>
2025-10-24 11:18:02 +0530

    [fix] small bug fixes

--- apps/web/src/app/scanpage/page.tsx
+++ apps/web/src/app/scanpage/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React, { useState, useEffect, useRef } from 'react';
 import { useRouter } from 'next/navigation';
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { Badge } from '@/components/ui/badge';
 import Footer from '@/components/footer';
 
